5505a3c83d227456927236850ffbac6465f0d2f6
Raymond Zhou <raymondzhou2019@gmail.com>
2020-05-30 12:40:13 -0400

    Added draw option

--- src/index.js
+++ src/index.js
@@ -143,6 +143,8 @@ class Game extends React.Component {
 
     if(winner) {
       status = 'Winner: ' + winner;
+    } else if (!current.squares.includes(null)) {
+      status = "Draw";
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
